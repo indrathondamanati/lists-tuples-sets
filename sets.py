5d31c60e8d7c4961e76aa2677ee0bf746ee3c7fe
indrathondamanati <tindra85143@gmail.com>
2026-09-17 15:24:12 +0530

    Refactor sets.py to include dictionary examples

--- sets.py
+++ sets.py
@@ -36,4 +36,57 @@ print("Hours:", hours)
 print("Minutes:", minutes)
 print("Seconds:", seconds)
 
+#access elements in dic
+std= {"name": "bhargavi", "age": 21, "grade": 85.5}
+print(std["name"])
+print(std["age"])
+print(std["grade"])
+
+#change value in dic
+std["age"] = 11
+print(std["age"])
+
+
+#add new data to a dic
+std["city"] = "vijaywada"
+print(std)
+
+#remove data
+std.pop("city")
+print(std)
+
+
+std= {"name": "bhargavi", "age": 21, "course": "python"}
+print(std.items())
+#items() return all key-value pairs
+print(std.get("name"))
+#get() returns the value of specified key
+
+std.update({"age": 17})
+#update() updates the value of the specified key
+
+print(std)
+
+std.pop("age")
+#pop() removes the specified key and its value
+
+print(std)
+
+#popitem() removes the last inserted key value pair
+std= {"name": "bhargavi", "age": 21, "course": "python"}
+std.popitem()
+print(std)
+
+std.setdefault("age", 21)
+print(std)
+
+#clear method
+std.clear()
+print(std)
+
+new_std = std.copy()
+print(new_std)
+
+
+
 
